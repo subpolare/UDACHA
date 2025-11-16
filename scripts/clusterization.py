8a5ce68fbc28aa509a0d1338c0f7ce63d184048e
Vladislav A. Nachatoi <135843012+subpolare@users.noreply.github.com>
2025-11-16 13:34:14 +0300

    fix: minor updates

--- scripts/clusterization.py
+++ scripts/clusterization.py
@@ -16,7 +16,7 @@ def main():
 
     K = np.loadtxt(args.king, dtype = np.float32)
     n_k = K.shape[0]
-    print(f'✔️ [1/5] Load KING matrix with shape {K.shape}')
+    print(f'✅\t[1/5]\tLoad KING matrix with shape {K.shape}')
 
     ids = pd.read_csv(args.king_id, sep = r'\s+', engine = 'python')
     if ids.shape[0] == n_k + 1:
@@ -27,20 +27,20 @@ def main():
         sample_id = ids.iloc[:, 0].astype(str).values
     else:
         sample_id = ids.iloc[:, 1].astype(str).values
-    print(f'✔️ [2/5] Load {len(sample_id)} sample IDs')
+    print(f'✅\t[2/5]\tLoad {len(sample_id)} sample IDs')
 
     np.fill_diagonal(K, 0.0)
     K[K < args.dist0] = 0.0
-    print(f'✔️ [3/5] Zero KING distances < {args.dist0}')
+    print(f'✅\t[3/5]\tZero KING distances < {args.dist0}')
 
     corr_dist = pdist(K, metric = 'correlation')
     Z = linkage(corr_dist, method = 'complete')
     labels = fcluster(Z, t = args.cut, criterion = 'distance')
-    print(f'✔️ [4/5] Perform hierarchical clustering with cut {args.cut}')
+    print(f'✅\t[4/5]\tPerform hierarchical clustering with cut {args.cut}')
 
     out = pd.DataFrame({'sample_id' : sample_id, 'geno_cluster_id' : labels.astype(int)})
     out.to_csv(args.output, sep = '\t', index = False)
-    print(f'✔️ [5/5] Save clusters to {args.output}')
+    print(f'✅\t[5/5]\tSave clusters to {args.output}')
 
 if __name__ == '__main__':
     main()
